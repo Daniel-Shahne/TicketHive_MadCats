b291adc2aafc0429b98d4b6bceef04dd5573b8c7
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-04-03 16:38:03 +0200

    Working on EventTicket dbcontext
    Installed all nugets and working on EventTicket db context. Only seeding data remains, havent migrated

---
+++ TicketHive_MadCats/Server/Data/EventTicketDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketHive_MadCats.Shared.Models;
+
+namespace TicketHive_MadCats.Server.Data
+{
+    public class EventTicketDbContext : DbContext
+    {
+        public EventTicketDbContext()
+        {
+            
+        }
+
+        public EventTicketDbContext(DbContextOptionsBuilder optionsBuilder) : base(optionsBuilder)
+        {
+            
+        }
+
+        public DbSet<EventModel> Events { get; set; }
+        public DbSet<TicketModel> Tickets { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EventTicketDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed data
+        }
+    }
+}
