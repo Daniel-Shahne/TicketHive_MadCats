9d798d45309a24ce9f543b6c76dc9b2066bbedeb
OscarKristland <113332057+OscarKristland@users.noreply.github.com>
2023-04-04 15:37:27 +0200

    Skapat metoder i EventRepo
    Skapat metoder som jag gissar kommer fungera.

---
+++ TicketHive_MadCats/Server/Repos/Repos/EventRepository.cs
@@ -0,0 +1,79 @@
+﻿using IdentityModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TicketHive_MadCats.Server.Data;
+using TicketHive_MadCats.Server.Repos.RepoInterfaces;
+using TicketHive_MadCats.Shared.Models;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TicketHive_MadCats.Server.Repos.Repos
+{
+    public class EventRepository : IEventRepo
+    {
+        public readonly EventTicketDbContext _context;
+
+        public EventRepository(EventTicketDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<EventModel?> CreateEvent(EventModel model)
+        {
+            var newEvent = new EventModel
+            {
+                Name = model.Name,
+                EventType = model.EventType,
+                MaxTickets = model.MaxTickets,
+                TicketPrice = model.TicketPrice,
+                Location = model.Location,
+                Date = model.Date,
+                ImageSrcs = model.ImageSrcs
+            };
+
+            _context.Events.Add(newEvent);
+            await _context.SaveChangesAsync();
+
+            return newEvent;
+        }
+
+        /// <summary>
+        /// Delete funktion som hittar event.id asynkront. om det inte är null så går den vidare och deletar,
+        /// sparar och returnerar true
+        /// Hittar den inte så returnerar den false
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteEventById(int id)
+        {
+            var eventToDelete = await _context.Events.FindAsync(id);
+            if(eventToDelete != null)
+            {
+                _context.Events.Remove(eventToDelete);
+                _context.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        public Task<List<EventModel>> GetAllEvents()
+        {
+            //Inkluderar tickets
+            return _context.Events.Include(e => e.Tickets).ToListAsync();
+            //annars
+            //return _context.Events.ToListAsync();
+        }
+
+        public Task<EventModel?> GetOneEventById(int id)
+        {
+            return _context.Events.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        //inkluderar tickets
+        public Task<EventModel?> GetOneEventByIdWithTickets(int id)
+        {
+            return _context.Events.Include(e => e.Tickets).FirstOrDefaultAsync(e => e.Id == id);
+        }
+    }
+}
---
+++ TicketHive_MadCats/Server/Repos/Repos/TicketRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TicketHive_MadCats.Server.Data;
+using TicketHive_MadCats.Server.Repos.RepoInterfaces;
+using TicketHive_MadCats.Shared.Models;
+
+namespace TicketHive_MadCats.Server.Repos.Repos
+{
+    public class TicketRepository : ITicketRepo
+    {
+        public readonly EventTicketDbContext _context;
+
+        public TicketRepository(EventTicketDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<TicketModel?> CreateTicket(TicketModel ticketModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTicket(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketModel>> GetAllTickets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TicketModel?> GetOneTicketById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
