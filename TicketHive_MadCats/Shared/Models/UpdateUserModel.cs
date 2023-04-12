3b0dcc41d99ce1ace35f636d3957a9dbe1b0c160
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-04-12 19:37:42 +0200

    Valid form done
    Wayy to tired to start on the controller, but that one shouldnt be an issue

---
+++ TicketHive_MadCats/Shared/Models/UpdateUserModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketHive_MadCats.Shared.Models
+{
+	public class UpdateUserModel
+	{
+
+		[JsonProperty("username")]
+		public string Username { get; set; } = null!;
+
+		[Required(ErrorMessage = "Must have a password")]
+		[MinLength(13, ErrorMessage = "Password must be atleast 13 characters long")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[@#£$%&!]).+$", ErrorMessage = "The {0} field must contain at least one capital letter and one of the following symbols: @ # £ $ % & !")]
+        [JsonProperty("password")]
+		public string Password { get; set; } = null!;
+
+		[Required(ErrorMessage = "Must have a country selected")]
+		[JsonProperty("country")]
+		public string Country { get; set; } = null!;
+	}
+}
