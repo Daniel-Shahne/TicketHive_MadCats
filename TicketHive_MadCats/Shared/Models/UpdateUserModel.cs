9e5076021560ab87d6600738ae38099defb5d000
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-04-13 13:03:19 +0200

    Userpage full functionality, not tested
    Only slightly tested but the values seem to update as expected.

--- TicketHive_MadCats/Shared/Models/UpdateUserModel.cs
+++ TicketHive_MadCats/Shared/Models/UpdateUserModel.cs
@@ -10,16 +10,16 @@ namespace TicketHive_MadCats.Shared.Models
 {
 	public class UpdateUserModel
 	{
-		[Required(ErrorMessage = "User name required / User not authenticated")]
+		// [Required(ErrorMessage = "User name required / User not authenticated")]
 		[JsonProperty("username")]
 		public string Username { get; set; } = null!;
 
-        [MinLength(13, ErrorMessage = "Passwords are atleast 13 characters long")]
+        // [MinLength(13, ErrorMessage = "Passwords are atleast 13 characters long")]
         [JsonProperty("currentPassword")]
 		public string? CurrentPassword { get; set; }
 
-		[MinLength(13, ErrorMessage = "Password must be atleast 13 characters long")]
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*[#£$%&!]).+$", ErrorMessage = "Password must contain at least one capital letter and one of the following symbols: # £ $ % & !")]
+		// [MinLength(13, ErrorMessage = "Password must be atleast 13 characters long")]
+        // [RegularExpression(@"^(?=.*[A-Z])(?=.*[#£$%&!]).+$", ErrorMessage = "Password must contain at least one capital letter and one of the following symbols: # £ $ % & !")]
         [JsonProperty("password")]
 		public string? Password { get; set; }
 
