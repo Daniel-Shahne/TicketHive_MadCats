b0152d1c8237729547a43711d553a5df8a443916
Daniel-Shahne <113332069+Daniel-Shahne@users.noreply.github.com>
2023-04-15 18:25:36 +0200

    fix

--- TicketHive_MadCats/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TicketHive_MadCats/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using TicketHive_MadCats.Server.Models;
 using TicketHive_MadCats.Shared.Statics;
 
@@ -13,12 +15,13 @@ namespace TicketHive_MadCats.Server.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<CustomUser> signInManager;
 
-        [Required(ErrorMessage = "Username is required")]
+		[Required(ErrorMessage = "Username is required")]
         [MinLength(5, ErrorMessage = "Username must be at least 5 characters")]
         public string? Username { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [MinLength(5, ErrorMessage = "Password must be at least 5 characters")]
+        [MinLength(13, ErrorMessage = "Password must be at least 13 characters")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[#¤$&!]).+$", ErrorMessage = "Password must contain at least one capital letter and one of the following symbols: # ¤ $ & !")]
         public string? Password { get; set; }
 
         [Required(ErrorMessage = "Please select a country")]
@@ -38,6 +41,15 @@ namespace TicketHive_MadCats.Server.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost()
         {
+            var userExists = await signInManager.UserManager.FindByNameAsync(Username) != null;
+            if (userExists)
+            {
+                Debug.WriteLine("User already exists");
+                ModelState.AddModelError(string.Empty, "A user with this username already exists.");
+                Debug.WriteLine("Added error to ModelState");
+                return Page();
+            }
+
             if (ModelState.IsValid)
             {
                 CustomUser newUser = new()
@@ -46,8 +58,8 @@ namespace TicketHive_MadCats.Server.Areas.Identity.Pages.Account
                     Country = SelectedCountry
                 };
 
-                var registerResult = await signInManager.UserManager.CreateAsync(newUser, Password!);
 
+                var registerResult = await signInManager.UserManager.CreateAsync(newUser, Password!);
                 if (registerResult.Succeeded)
                 {
 
